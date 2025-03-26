7ec7fd62b00f94bdf242adc91156162e507393d2
Walalee Mauenjit <walalee.mauenjit@gmail.com>
2025-03-26 22:33:27 +0700

    backend connect

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import SplashScreen from './loadingscreen'; 
 import Homescreen from './Homescreen';  // ใช้ชื่อที่ถูกต้อง
-import CouponScreen from './CouponScreen';
+import CouponScreen from './components/CouponScreen';
 import CouponDetailScreen from './CouponDetailScreen';
 
 const App = () => {
